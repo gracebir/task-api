6dcaed4dd32e373f9be1e28e39389b3261e95d28
graceDev1 <grace.devolop@gmail.com>
2021-08-04 10:47:40 +0200

    done with crud operation service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 
-
+//mongodb+srv://rachoder:Firewall12@mern.v8ddv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 @Module({
-  imports: [TaskModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/task')],
+  imports: [TaskModule,MongooseModule.forRoot('mongodb://127.0.0.1:27017/task',{useFindAndModify:false})],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -5,7 +5,33 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class TaskService {
-    constructor(@InjectModel('Task') private readonly taskModel: Model<ITask>){
-        
+    constructor(@InjectModel('Task') private readonly taskModel: Model<ITask>){}
+
+    async addTaskToDatabase(title:string){
+        const newTask = await new this.taskModel({title, completed: false});
+        return newTask.save();
+    }
+
+    async fetchTask(){
+        return await this.taskModel.find();
+    }
+
+    async modifyTask(id:string){
+        let newTask = {
+            completed: true
+        }
+
+        const findTask = await this.taskModel.findById({_id:id});
+        if(findTask.completed === true){
+            newTask.completed = false
+        } else {
+            newTask.completed = true
+        }
+
+        return await this.taskModel.findByIdAndUpdate({_id:id}, newTask);
+    }
+
+    async remoteTask(id:string){
+        return await this.taskModel.findByIdAndDelete({_id:id});
     }
 }
